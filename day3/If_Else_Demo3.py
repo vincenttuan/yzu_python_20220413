f220fdf607d317cb13cede378cf79825f7aff34e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-04-27 19:07:12 +0800

    Add files via upload

--- day3/If_Else_Demo3.py
+++ day3/If_Else_Demo3.py
@@ -7,3 +7,17 @@ import random as r
 bmi = r.randint(10, 35)
 print('bmi:', bmi)
 # 請利用 if-elif-else 判斷 bmi 是 "過輕" "正常" "過重"
+# 第一種寫法:
+if bmi <= 18:
+    print('過輕')
+elif bmi > 23:
+    print('過重')
+else:
+    print('正常')
+# 第二種寫法:
+if 18 < bmi <= 23:
+    print('正常')
+elif bmi > 23:
+    print('過重')
+else:
+    print('過輕')
