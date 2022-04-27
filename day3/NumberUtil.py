140d9c0a5770b9bd00aeddab5cf027f795909c2c
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-04-27 21:09:22 +0800

    Add files via upload

--- day3/NumberUtil.py
+++ day3/NumberUtil.py
@@ -1,6 +1,22 @@
 # 處理數字的涵式集
 # 小數點 n 位 + 千分號
-def get(num, n):
+# n=1 小數點 n 位 預設值是 1
+# mark=True 表示是否要加上千分號
+def get(num, n=1, mark=True):
     num = int(num * 10**n)/10**n
-    result = format(num, ",")
+    if mark: # if mark == True
+        result = format(num, ",")
+    else:
+        result = num
     return result
+
+# 處理股價的交易成本
+# price 股票價格
+# shares 股票股數 (=1000)
+# rate 手續費率 (=0.001425)
+def getBuyCost(price, shares=1000, rate=0.001425):
+    cost = price * shares
+    cost = cost + (cost * rate)
+    return cost
+
+
