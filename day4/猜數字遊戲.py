94f99a346769708ba751f40b99ffe73800eb1da7
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-04 20:59:30 +0800

    Add files via upload

--- day4/猜數字遊戲.py
+++ day4/猜數字遊戲.py
@@ -10,17 +10,34 @@ min  max
 36 ~ 41
 38
 '''
-ans = 38
+import random as r
 min = 0
 max = 100
+#ans = r.randint(1, 99)
+ans = r.randint(min+1, max-1)
 while True:
+    # user 猜數字
     user_guess = int(input('使用者 %d ~ %d 之間猜一數字: ' % (min, max)))
-    # 判斷是否有猜對 ?
+    # 判斷 user 是否有猜對 ?
     if user_guess < ans:
         min = user_guess
     elif user_guess > ans:
         max = user_guess
     else:
-        print('恭喜答對')
+        print('恭喜 user 答對')
         break
+    #-------------------------------------------------------------------
+    # pc 猜數字
+    # min  max    min+1   max-1
+    # 20 ~ 65 ==> 21   ~  64
+    pc_guess = r.randint(min+1, max-1)
+    print('pc 猜:', pc_guess)
+    if pc_guess < ans:
+        min = pc_guess
+    elif pc_guess > ans:
+        max = pc_guess
+    else:
+        print('恭喜 pc 答對')
+        break
+
 
