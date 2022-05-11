10f43bd393741223d01c612b86e382965c038579
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-11 19:23:02 +0800

    Update LineTest.py

--- day5/LineTest.py
+++ day5/LineTest.py
@@ -2,10 +2,11 @@ import day5.Line as line
 
 if __name__ == '__main__' :
     # 測試文字傳送
-    #line.lineNotifyText('Helle World')
+    line.lineNotifyText('Helle World')
 
     # 測試 sticker 傳送
     line.lineNotifySticker(789, 10887)
 
-    
+    # 測試本地端圖片上傳 傳送
+    line.lineNotifyLocalImage('f16.jpg')
 
