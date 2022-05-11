21556277f28033ef8b7eb9a8a20ade897c76b2fb
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-11 21:11:40 +0800

    Add files via upload

--- day5/撲克牌洗牌II.py
+++ day5/撲克牌洗牌II.py
@@ -1,12 +1,16 @@
+import random as r
 poker = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 #位置     0    1    2    3    4    5    6    7    8    8     10   11   12
 #洗牌前
 print(poker)
 #洗牌
-p1 = 3
-p2 = 5
-num = poker[p1]
-poker[p1] = poker[p2]
-poker[p2] = num
+for i in range(5):
+    print('i 的值 =', i)
+    lens = len(poker) - 1
+    p1 = r.randint(0, lens)
+    p2 = r.randint(0, lens)
+    num = poker[p1]
+    poker[p1] = poker[p2]
+    poker[p2] = num
 #洗牌後
 print(poker)
