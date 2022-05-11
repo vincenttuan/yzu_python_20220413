e026e3b8c0c28e05afdc64561a76609822f8e503
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-11 20:05:29 +0800

    Update LineTest.py

--- day5/LineTest.py
+++ day5/LineTest.py
@@ -8,5 +8,8 @@ if __name__ == '__main__' :
     line.lineNotifySticker(789, 10887)
 
     # 測試本地端圖片上傳 傳送
-    line.lineNotifyLocalImage('f16.jpg')
+    line.lineNotifyLocalImage('f18.jpg')
 
+    # 測試雲端端圖片 傳送
+    imageURI = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/A-10_Thunderbolt_II_In-flight-2.jpg/325px-A-10_Thunderbolt_II_In-flight-2.jpg'
+    line.lineNotifyWebImage(imageURI)
