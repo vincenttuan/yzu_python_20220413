337a8c2738ab6e0686879b47022e4c6fdb2044e0
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-18 19:48:17 +0800

    Add files via upload

---
+++ day6/練習split.py
@@ -0,0 +1,17 @@
+# 練習 split
+# 將字串分割成數組
+#      姓名  年齡 身高  體重
+str = "John,18, 170, 60"
+print(str)
+datas = str.split(",")
+print(datas, type(datas))
+
+print(datas[0].strip())  # 姓名
+print(datas[1].strip())  # 年齡
+print(datas[2].strip())  # 身高
+print(datas[3].strip())  # 體重
+h = int(datas[2])
+w = int(datas[3])
+bmi = w / (h/100)**2
+print(bmi)
+
