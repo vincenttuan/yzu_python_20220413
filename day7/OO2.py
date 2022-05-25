1b6c37bcc4a3f67de43b84c0c20ef8ea782bc1cb
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-25 20:42:17 +0800

    Add files via upload

---
+++ day7/OO2.py
@@ -0,0 +1,16 @@
+class BMI:
+    h = 0  # 公有變數
+    w = 0  # 公有變數
+    __bmi = 0 # 私有
+
+    # 計算 bmi 值
+    def calcBMI(self):
+        self.__bmi = self.w / (self.h/100)**2
+
+    # 取得 bmi 值
+    def getBMI(self):
+        return self.__bmi
+
+    def __str__(self):
+        return float(self.h) + ', ' + float(self.w) + ', ' + float(self.__bmi)
+
