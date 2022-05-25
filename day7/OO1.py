8ff9ddc473e1758882f8ddafd78b8a8a3cd2eaa5
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-05-25 19:59:04 +0800

    Update OO1.py

--- day7/OO1.py
+++ day7/OO1.py
@@ -4,6 +4,10 @@ class Human:
     name = ''  # 屬性/欄位/變數
     age = 0
     sex = ''
+    # 將物件內容以自訂字串格式輸出
+    def __str__(self) -> str:
+        return self.name + ', ' + str(self.age) + ', ' + self.sex
+
 
 if __name__ == '__main__':
     # 建立一個物件
@@ -13,4 +17,7 @@ if __name__ == '__main__':
     h2 = Human()
     h2.name = 'Anita';h2.age = 19;h2.sex = '女'
 
+    #print(h1, h1.name, h1.age, h1.sex)
+    #print(h2, h2.name, h2.age, h2.sex)
     print(h1)
+    print(h2)
