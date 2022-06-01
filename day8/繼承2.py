24695cb8baa6c8131f42a5950904c45c0aa195d6
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-01 19:12:07 +0800

    Add files via upload

--- day8/繼承2.py
+++ day8/繼承2.py
@@ -5,12 +5,18 @@ class Person:
         self.age = age
         self.sex = sex
 
+    def __str__(self):
+        return 'name: %s age: %d sex: %s ' % (self.name, self.age, self.sex)
+
 class Student(Person):
-    grade = 0
     def __init__(self, name, age, sex, grade):
-        # super 在此指的就是 Person
-        super.__init__(name, age, sex)  # 調用 Person 的 __init__ 方法
+        # super() 在此指的就是 Person 物件
+        super().__init__(name, age, sex)  # 調用 Person 的 __init__ 方法
         self.grade = grade
+
+    def __str__(self):
+        return super().__str__() + 'grade: %d' % self.grade
 
 if __name__ == '__main__':
     student = Student('Vincent', 18, 'M', 8)
+    print(student)
