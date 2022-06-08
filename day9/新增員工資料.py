6e04778a7f3ad2610da0429c2eec873059c034c1
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-08 21:22:55 +0800

    Add files via upload

--- day9/新增員工資料.py
+++ day9/新增員工資料.py
@@ -11,4 +11,10 @@ sql = '''
 sql = sql.strip() % (id, ename, esalary, did)
 print(sql)
 
-# conn = sqlite3.connect('demo.db')
+conn = sqlite3.connect('demo.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+rowcount = cursor.rowcount  # 異動筆數
+print('新增', rowcount, '筆成功')
+conn.commit()  # 提交
+conn.close()
--- day9/查詢員工資料.py
+++ day9/查詢員工資料.py
@@ -6,6 +6,10 @@ sql = 'select id, ename, esalary, did from employee'
 cursor.execute(sql)  # 丟給 sqlite 去執行 sql 語句
 rows = cursor.fetchall()  #得到因 sql 指令的紀錄列表
 # 透過 for-loop 將資料逐筆顯示
+print('全部查詢')
+for row in rows:
+    print(row[0], row[1], row[2], row[3])
+
 print('薪資 >= 100000')
 for row in rows:
     if row[2] >= 100000:
