b5f0169dee56a4d5b9b5105c0571fef8f565e73a
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-15 20:05:16 +0800

    Add files via upload

--- day10/OpenCV_Test.py
+++ day10/OpenCV_Test.py
@@ -1,5 +1,8 @@
 import cv2
 
+# 定義臉部偵測特徵檔
+face_cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
+
 # 設定攝像鏡頭(0, 1, 2, ...)
 cap = cv2.VideoCapture(0)
 
@@ -15,7 +18,20 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 while True:
     # 捕捉 frame
     ret, frame = cap.read()
-    print(frame)
+    #print(frame)
+
+    # 偵測人臉
+    faces = face_cascade.detectMultiScale(
+        frame, # 待檢測圖片
+        scaleFactor=1.1,  # 檢測粒度
+        minNeighbors=5,  # 每個目標至少要檢測幾次以上才認定
+        minSize=(30, 30),  # 搜尋最小尺寸
+        flags=cv2.CASCADE_SCALE_IMAGE  # 正常比例檢測
+    )
+
+    amount = len(faces)
+    if(amount > 0):
+        print('找到 %d 個人臉' % amount)
 
     # 將 frame 資料顯示
     cv2.imshow('My camera', frame)
